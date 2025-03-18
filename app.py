5b939d0ea6ff31c930b1ac44781af56f769d3a95
ketan ohal <mailmequicker@gmail.com>
2025-03-18 23:39:01 +0530

    Deploying to Vercel 8

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ rent_collection = db["rent"]  # Collection storing rent details ✅ ADDED THIS
 expenses_collection = db["expenses"]
 
 
-UPLOAD_FOLDER = 'static/uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+UPLOAD_FOLDER = '/tmp/uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
