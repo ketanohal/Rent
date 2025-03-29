2ab355a9f9c730d3b6d63f470dbef7d84473559c
KETAN OHAL <53792027+ketanohal@users.noreply.github.com>
2025-03-29 11:44:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -691,4 +691,5 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host="0.0.0.0", port=5000, debug=True)  # ✅ Ensure the app listens on port 5000
+
